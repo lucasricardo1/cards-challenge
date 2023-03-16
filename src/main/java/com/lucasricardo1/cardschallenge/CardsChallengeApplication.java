c1878060feb418ccc70cd9ee2b373d30673cd169
user_pumpkin07 <user_pumpkin07@tribanco.com.br>
2023-03-15 22:37:48 -0300

    First commit

---
+++ src/main/java/com/lucasricardo1/cardschallenge/CardsChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.lucasricardo1.cardschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardsChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasricardo1/cardschallenge/CardsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasricardo1.cardschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
