b7003df05ceef799734e541a2204b39b59f15475
user_pumpkin07 <user_pumpkin07@tribanco.com.br>
2023-03-20 01:56:51 -0300

    Fix entities and repositories

--- src/main/java/com/lucasricardo1/cardschallenge/repositories/ShuffleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lucasricardo1.cardschallenge.repositories;
-
-import com.lucasricardo1.cardschallenge.entities.ShuffleEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ShuffleRepository extends CrudRepository<ShuffleEntity, String> {
-}
